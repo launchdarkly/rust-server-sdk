0cc6bd2035878b07feea07189c0285a43de95935
Sam Stokes <sstokes@launchdarkly.com>
2019-03-14 15:11:47 -0700

    refactor HTTP client into a lib

---
+++ src/bin/hack.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+use futures::future::Future;
+use futures::stream::Stream;
+
+use ldclient::eventsource::Client as EV;
+
+fn main() {
+    println!("Connecting...");
+
+    let sdk_key = env::var("LAUNCHDARKLY_SDK_KEY").expect("Please set LAUNCHDARKLY_SDK_KEY");
+
+    let client = EV::for_url("https://stream-stg.launchdarkly.com/all")
+        .header("Authorization", &sdk_key)
+        .build();
+
+    let event_stream = client.stream();
+
+    tokio::run(
+        event_stream
+            .for_each(|event| {
+                println!("got a event: {:?}", event);
+                Ok(())
+            })
+            .map_err(|_| println!("error!")),
+    );
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod eventsource;
+
 #[cfg(test)]
 mod tests {
     #[test]
