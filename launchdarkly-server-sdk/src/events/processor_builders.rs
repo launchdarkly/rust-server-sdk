2d63e77e00320ceb7a8edaf1574d58cb4091ba53
Oscar Franco <ospfranco@gmail.com>
2025-03-13 12:11:46 +0100

    Also patch processor builder

--- launchdarkly-server-sdk/src/events/processor_builders.rs
+++ launchdarkly-server-sdk/src/events/processor_builders.rs
@@ -9,6 +9,12 @@ use hyper::service::Service;
 use hyper::Uri;
 #[cfg(feature = "rustls")]
 use hyper_rustls::HttpsConnectorBuilder;
+#[cfg(feature = "native-certs")]
+use hyper::client::HttpConnector;
+#[cfg(feature = "native-certs")]
+use hyper_rustls::HttpsConnector;
+#[cfg(feature = "native-certs")]
+use rustls::ClientConfig;
 use launchdarkly_server_sdk_evaluation::Reference;
 use thiserror::Error;
 use tokio::io::{AsyncRead, AsyncWrite};
@@ -88,6 +94,42 @@ pub struct EventProcessorBuilder<C> {
     // diagnostic_recording_interval: Duration
 }
 
+#[cfg(feature = "native-certs")]
+fn create_https_connector_with_native_certs() -> HttpsConnector<HttpConnector> {
+    // Load native certs
+    let mut root_store = rustls::RootCertStore::empty();
+    match rustls_native_certs::load_native_certs() {
+        Ok(certs) => {
+            for cert in certs {
+                root_store
+                    .add(&rustls::Certificate(cert.0))
+                    .unwrap_or_else(|e| {
+                        eprintln!("Failed to add certificate: {}", e);
+                    });
+            }
+            println!("Added {} certificates", root_store.len());
+        }
+        Err(e) => {
+            eprintln!("Failed to load native certificates: {}", e);
+            // Fall back to an empty store, which will likely fail connections
+        }
+    }
+
+    // Create TLS config
+    let tls_config = ClientConfig::builder()
+        .with_safe_defaults()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+
+    // Build the HTTPS connector
+    HttpsConnectorBuilder::new()
+        .with_tls_config(tls_config)
+        .https_only()
+        .enable_http1()
+        .enable_http2()
+        .build()
+}
+
 impl<C> EventProcessorFactory for EventProcessorBuilder<C>
 where
     C: Service<Uri> + Clone + Send + Sync + 'static,
@@ -124,20 +166,36 @@ where
             } else {
                 #[cfg(feature = "rustls")]
                 {
-                    let connector = HttpsConnectorBuilder::new()
-                        .with_native_roots()
-                        .https_or_http()
-                        .enable_http1()
-                        .enable_http2()
-                        .build();
+                    #[cfg(feature = "native-certs")]
+                    {
+                        let connector = create_https_connector_with_native_certs();
+                        Ok(Arc::new(HyperEventSender::new(
+                            connector,
+                            hyper::Uri::from_str(url_string.as_str()).unwrap(),
+                            sdk_key,
+                            default_headers,
+                            self.compress_events,
+                        )))
+                    }
 
-                    Ok(Arc::new(HyperEventSender::new(
-                        connector,
-                        hyper::Uri::from_str(url_string.as_str()).unwrap(),
-                        sdk_key,
-                        default_headers,
-                        self.compress_events,
-                    )))
+                    #[cfg(not(feature = "native-certs"))]
+                    {
+
+                        let connector = HttpsConnectorBuilder::new()
+                            .with_native_roots()
+                            .https_or_http()
+                            .enable_http1()
+                            .enable_http2()
+                            .build();
+    
+                        Ok(Arc::new(HyperEventSender::new(
+                            connector,
+                            hyper::Uri::from_str(url_string.as_str()).unwrap(),
+                            sdk_key,
+                            default_headers,
+                            self.compress_events,
+                        )))
+                    }
                 }
                 #[cfg(not(feature = "rustls"))]
                 Err(BuildError::InvalidConfig(
